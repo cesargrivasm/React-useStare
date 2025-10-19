c470dc0ee6ec16f6464f35bf0b9f0bf5eb0eba2b
cesargrivasm <cesarg.rivasm@gmail.com>
2025-10-19 14:19:41 -0400

    third commit final revision

--- src/UseState.tsx
+++ src/UseState.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseState = () => {
+    const [count, setCount]=useState(0);
   return (
     <div>
-      Use State Component
+      <h1>Use State Component</h1>
+      {count}
+      <hr/>
+      <button onClick={()=>{
+        setCount(count + 1);
+      }}>Incrementar</button>
+
+      <button onClick={()=>{
+        setCount(count - 1);
+      }}>Decrementar</button>
+
+      <button onClick={()=>{
+        setCount(0);
+      }}>Reset</button>
+
     </div>
   )
 }
